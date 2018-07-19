afa9f4bf86e95396f98ed89f2913da60cb7be738
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-07-19 22:33:04 +0530

    Committing changes on pushing angular component to global npm registry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddressModule } from './modules/address/address.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AddressModule
   ],
-  providers: [],
+  providers: [], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
